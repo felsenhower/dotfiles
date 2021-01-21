a5413fcdd000400f79fc555c793af4b3f4f6f84c
Ruben Felgenhauer <4felgenh@informatik.uni-hamburg.de>
2021-01-21 18:22:03 +0100

    Add function "installed"

--- public/shelltools/1200_thefuck.sh
+++ public/shelltools/1200_thefuck.sh
@@ -1,2 +1,5 @@
 # For plugin thefuck https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/thefuck
-which thefuck >/dev/null 2>/dev/null && eval $(thefuck --alias) || true
+if installed thefuck ; then
+  eval $(thefuck --alias)
+fi
+
--- public/shelltools/1600_github_cli.zsh
+++ public/shelltools/1600_github_cli.zsh
@@ -1,1 +1,4 @@
-which gh >/dev/null 2>/dev/null && eval "$(gh completion -s zsh)" || true 
+if installed gh ; then
+  eval "$(gh completion -s zsh)"
+fi
+
