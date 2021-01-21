85a13d8f77d66ee6100820679a62f401562725a2
Ruben Felgenhauer <4felgenh@informatik.uni-hamburg.de>
2021-01-21 18:17:29 +0100

    Also suppress output here

--- public/shelltools/2000_common.sh
+++ public/shelltools/2000_common.sh
@@ -4,7 +4,7 @@ bell() {
 #  notify-send -i preferences-desktop-notification-bell -a "bell" "Command finished" "$(echo "$@")"
 }
 
-if which nvim >/dev/null ; then
+if which nvim >/dev/null 2>/dev/null ; then
   alias vim="nvim"
   alias vi="nvim"
   alias vimdiff='nvim -d'
@@ -13,7 +13,7 @@ elif which vim >/dev/null ; then
   export EDITOR=vim
 fi
 
-if which git >/dev/null ; then
+if which git >/dev/null 2>/dev/null ; then
   git config --global user.name 'Ruben Felgenhauer'
   git config --global user.email '4felgenh@informatik.uni-hamburg.de'
 fi
