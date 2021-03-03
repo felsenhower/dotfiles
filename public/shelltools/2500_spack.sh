48264e725f498ca8c2d90cd273a278ee828d0995
Ruben Felgenhauer <Ruben.Felgenhauer@uni-hamburg.de>
2021-03-03 22:21:48 +0100

    Make load_spack an alias instead of a function

--- public/shelltools/2500_spack.sh
+++ public/shelltools/2500_spack.sh
@@ -1,5 +1,3 @@
 export SPACK_PATH="$HOME"'/.local/share/spack'
 
-function load_spack() {
-  source "$SPACK_PATH"'/share/spack/setup-env.sh' 
-}
+alias load_spack="source $SPACK_PATH"'/share/spack/setup-env.sh'
